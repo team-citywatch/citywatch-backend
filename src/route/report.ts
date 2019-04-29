5ba210b5a14a68a2943e0b99f0afa5823ab50d29
Jeong Ukjae <JeongUkJae@gmail.com>
2019-04-29 13:22:20 -0700

    add report api (#8)

--- src/route/report.ts
+++ src/route/report.ts
@@ -1,41 +1,120 @@
 import * as express from "express";
+import { Op } from "sequelize";
+
+import { Report } from "../database/report.model";
+
+const REPORT_PER_PAGE = 10;
 
 export class ReportRoute {
   public static registerServerRoute(app: express.Application) {
-    app.get("/report/", (_, res) => {
+    app.get("/report/", async (req, res) => {
       // for fetch report
-      res.status(501);
-      res.send("Not Implemented")
-    })
+      try {
+        const keyword = req.query.keyword;
+        let page = parseInt(req.query.page, 10) || 1;
 
-    app.post("/report/", (_, res) => {
-      // for create report
-      res.status(501);
-      res.send("Not Implemented")
+        const reports = await Report.findAll(
+          keyword && {
+            limit: REPORT_PER_PAGE * (page - 1),
+            offset: REPORT_PER_PAGE,
+            where: {
+              content: {
+                [Op.like]: keyword
+              }
+            }
+          }
+        );
+
+        res.send({
+          reports
+        });
+      } catch (reason) {
+        res.status(500);
+        res.send({ message: `database error: ${reason}` });
+      }
     });
 
-    app.delete("/report/:report", (_, res) => {
-      // for delete report
-      res.status(501);
-      res.send("Not Implemented")
-    })
+    app.post("/report/", async (req, res) => {
+      // for create report
+      // TODO : auth
+      const report = Report.build({
+        ...req.body,
+        createdAt: undefined,
+        updatedAt: undefined,
+        id: undefined
+      });
 
-    app.put("/report/:report", (_, res) => {
+      try {
+        await report.save();
+        res.send({ message: "success" });
+      } catch (reason) {
+        res.status(400);
+        res.send({ message: reason });
+      }
+    });
+
+    app.delete("/report/:report", async (req, res) => {
+      // for delete report
+      const reportId = req.params.report;
+
+      try {
+        const report = await Report.findOne({
+          where: {
+            id: reportId
+          }
+        });
+        if (report) {
+          await report.destroy();
+          res.send({ message: "success" });
+        } else {
+          res.status(400);
+          res.send({ message: `Report ${reportId} does not exist` });
+        }
+      } catch (reason) {
+        res.status(500);
+        res.send({ message: `database error: ${reason}` });
+      }
+    });
+
+    app.put("/report/:report", async (req, res) => {
       // for update report
-      res.status(501);
-      res.send("Not Implemented")
-    })
+      const reportId = req.params.report;
+      try {
+        const report = await Report.findOne({
+          where: {
+            id: reportId
+          }
+        });
+
+        if (report) {
+          let payload = req.body || {};
+          delete payload["id"];
+          delete payload["createdAt"];
+          delete payload["updatedAt"];
+
+          await report.update(payload);
+          res.send(200);
+          res.send({ message: "success" });
+        } else {
+          res.status(400);
+          res.send({ message: `Report ${reportId} does not exist` });
+        }
+      } catch (reason) {
+        res.status(500);
+        res.send({ message: `database error: ${reason}` });
+      }
+    });
 
     app.delete("/report/:report/image", (_, res) => {
       // for delete an image in report
       res.status(501);
-      res.send("Not Implemented")
-    })
+      res.send("Not Implemented");
+    });
 
     app.post("/report/:report/image", (_, res) => {
       // for update an image in report
       res.status(501);
-      res.send("Not Implemented")
-    })
+      res.send("Not Implemented");
+    });
   }
 }
